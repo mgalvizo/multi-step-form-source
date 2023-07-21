b3fbb15ddc117d24e363da94cff2a264642b0836
Miguel Garcia <miguelangelgarciaalvizo@hotmail.com>
2023-07-21 13:09:11 -0600

    edit README.md and add styles for the page not found

--- src/components/pages/PageNotFound.component.tsx
+++ src/components/pages/PageNotFound.component.tsx
@@ -11,14 +11,16 @@ const PageNotFound = () => {
     };
 
     return (
-        <StyledPageNotFound className="component">
-            <div className="component__content">
-                <StyledHeading as="h1">
-                    We can't find the page you're looking for.
-                </StyledHeading>
-                <Button type="button" onClick={goBack} kind="back">
-                    Go Back
-                </Button>
+        <StyledPageNotFound>
+            <div className="app__layout">
+                <div className="app-layout__content">
+                    <StyledHeading as="h1">
+                        We can't find the page you're looking for.
+                    </StyledHeading>
+                    <Button type="button" onClick={goBack} kind="back">
+                        Go Back
+                    </Button>
+                </div>
             </div>
         </StyledPageNotFound>
     );
--- src/components/styled/Pages/PageNotFound.styled.ts
+++ src/components/styled/Pages/PageNotFound.styled.ts
@@ -1,5 +1,27 @@
 import styled from 'styled-components';
 
-const StyledPageNotFound = styled.main``;
+const StyledPageNotFound = styled.main`
+    padding: var(--web-padding-m);
+
+    h1 {
+        margin-bottom: var(--web-margin-m);
+    }
+
+    // 768px
+    @media only screen and (min-width: 48em) {
+        display: flex;
+        flex-flow: row wrap;
+        justify-content: center;
+        align-items: center;
+        padding-top: 0;
+        padding-right: var(--web-padding-l);
+        padding-bottom: 0;
+        padding-left: var(--web-padding-l);
+
+        .app__layout {
+            flex-basis: var(--max-content-width);
+        }
+    }
+`;
 
 export default StyledPageNotFound;
